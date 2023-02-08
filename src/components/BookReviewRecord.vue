6bed62539510c54eada61ab29466ffda6f3fea38
RyoShunkyoku <ryo.shunkyoku.75s@jp.nssol.nipponsteel.com>
2023-02-08 17:21:41 +0900

    book review component

---
+++ src/components/BookReviewRecord.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="p-5 w-56 border-2 border-blue-300 rounded-md mx-auto">
+      <div>名前: {{ name }}</div>
+      <div>所属: {{ department }}</div>
+      <div>職種: {{ occupation }}</div>
+      <div>入社年分: {{ joinedYear }}</div> 
+      <div>コメント: {{ comment }}</div>
+      <div class="text-center mt-12">
+        <v-rating
+          v-model="rating"
+          color="yellow darken-3"
+          background-color="grey darken-1"
+          large
+        ></v-rating>
+      </div>
+    </div>
+  </template>
+
+  <script>
+  export default ({
+    props: ['name', 'department', 'occupation', 'joinedYear', 'phoneNumber'],
+  })
+  </script>
