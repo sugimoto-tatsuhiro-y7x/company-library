72e2c08f84117ed1b69205a1300f69877e986606
sugimoto.tatsuhiro <sugimoto.tatsuhiro.y7x@jp.nssol.nipponsteel.com>
2023-02-08 17:06:15 +0900

    avatarをクリックするとユーザ詳細画面に移動

--- src/views/BookList.vue
+++ src/views/BookList.vue
@@ -24,12 +24,16 @@ import HelloWorld from '@/components/HelloWorld.vue'
                 <h1>社内図書システム</h1>
             </v-col>
             <v-col>
-                    <router-link to="/foo">書籍詳細ダイアログ</router-link>|
-                    <router-link to="/UserDetail">UserDetail</router-link>|
-                    <router-link to="/">本一覧画面</router-link>
+                <router-link to="/foo">書籍詳細ダイアログ</router-link>|
+                <router-link to="/UserDetail">UserDetail</router-link>|
+                <router-link to="/">本一覧画面</router-link>
             </v-col>
             <v-col>
-                <v-avatar color="primary" size="56">TS</v-avatar>
+                <router-link to="/UserDetail">
+                    <v-avatar size="56">
+                        <v-img src="../assets/kanako.png"></v-img>
+                    </v-avatar>
+                </router-link>|
             </v-col>
 
         </v-row>
