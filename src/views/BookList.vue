1c88e1239f04388b1fc488c64134711f468d067b
RyoShunkyoku <ryo.shunkyoku.75s@jp.nssol.nipponsteel.com>
2023-02-09 16:46:18 +0900

    コンフリクト解消

--- src/views/BookList.vue
+++ src/views/BookList.vue
@@ -19,19 +19,11 @@ const cancelSearching = () => {
 };
 
 const fetcher = (search) => {
-<<<<<<< HEAD
-  let url = new URL("https://www.googleapis.com/books/v1/volumes")
-
-  const param = {
-    q: search
-  }
-=======
   let url = new URL("https://www.googleapis.com/books/v1/volumes");
 
   const param = {
     q: search,
   };
->>>>>>> a90e3b7022327ec21fe2ce2848de6360cdef0a2f
 
   url.search = new URLSearchParams(param).toString();
   return fetch(url)
@@ -43,21 +35,7 @@ const fetcher = (search) => {
     });
 };
 
-<<<<<<< HEAD
-  url.search = new URLSearchParams(param).toString()
-  return fetch(url)
-    .then((res) => {
-      return res && res.json()
-    }).then((data) => {
-      return data
-    })
-}
-
-const { data, error } = useSWRV("cicd", fetcher)
-
-=======
 const { data, error } = useSWRV("cicd", fetcher);
->>>>>>> a90e3b7022327ec21fe2ce2848de6360cdef0a2f
 </script>
 
 <template>
@@ -81,24 +59,6 @@ const { data, error } = useSWRV("cicd", fetcher);
       </v-col>
     </v-row>
 
-<<<<<<< HEAD
-
-    <!-- 検索フォーム -->
-    <v-form>
-      <v-text-field prepend-inner-icon="mdi-magnify" label="書籍検索" placeholder="書籍名等を入力してください"
-        clear-icon="mdi-close-circle" clearable></v-text-field>
-    </v-form>
-
-    <h2>New</h2>
-    <BookSlideGroups :books=books></BookSlideGroups>
-    <h2>おすすめ</h2>
-    <BookSlideGroups :books=books></BookSlideGroups>
-
-    <!-- 本一覧 -->
-
-    <div v-for="book in books" :key="book">
-      <BookListItem :book=book />
-=======
     <!-- 検索フォーム -->
     <v-row no-gutters>
       <v-col cols="1" v-show="searching">
@@ -108,13 +68,8 @@ const { data, error } = useSWRV("cicd", fetcher);
       </v-col>
       <v-col :cols="searching ? 10 : 11">
         <v-form>
-          <v-text-field
-            prepend-inner-icon="mdi-magnify"
-            label="書籍検索"
-            placeholder="書籍名等を入力してください"
-            clear-icon="mdi-close-circle"
-            clearable
-          ></v-text-field>
+          <v-text-field prepend-inner-icon="mdi-magnify" label="書籍検索" placeholder="書籍名等を入力してください"
+            clear-icon="mdi-close-circle" clearable></v-text-field>
         </v-form>
       </v-col>
       <v-col cols="1">
@@ -133,7 +88,6 @@ const { data, error } = useSWRV("cicd", fetcher);
 
     <div v-for="book in books" :key="book">
       <BookListItem :book="book" />
->>>>>>> a90e3b7022327ec21fe2ce2848de6360cdef0a2f
     </div>
     <!-- <div v-if="error">failed to load</div>
         <div v-if="!data">loading...</div>
@@ -142,9 +96,5 @@ const { data, error } = useSWRV("cicd", fetcher);
                 <BookListItem :book=book />
             </div>
         </div> -->
-<<<<<<< HEAD
-
-=======
->>>>>>> a90e3b7022327ec21fe2ce2848de6360cdef0a2f
   </v-container>
 </template>
