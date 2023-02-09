ac9fc7341d9e65f326857f9ac5ffc154f33e61a5
RyoShunkyoku <ryo.shunkyoku.75s@jp.nssol.nipponsteel.com>
2023-02-09 14:33:01 +0900

    BookReviewRec Component

--- src/components/BookReviewRecord.vue
+++ src/components/BookReviewRecord.vue
@@ -3,26 +3,34 @@
     class="elevation-16 mx-auto"
     width="800"
   >
-    <v-list-item>
-      <v-list-item-avatar>
+    <v-row>
+      <v-col cols="1">
+        <v-list-item-avatar>
         <v-img :src="item.avatar" max-height="50" max-width="50"></v-img>
-      </v-list-item-avatar>
-      <div>
-        <div>営業 {{ occupation }}</div>
-        <div>2020年入社 {{ joinedYear }}</div> 
-      </div>
-  <div class="text-left mt-12">
-        <v-rating
-          v-model="item.rating"
-          color="yellow darken-3"
-          background-color="grey darken-1"
-          size="24"
-          large
-        ></v-rating>
-    </div>
+        </v-list-item-avatar>
+      </v-col>
+      <v-col>
+          <div>{{ occupation }}営業</div>
+          <div>{{ joinedYear }}2020年入社</div> 
+      </v-col>
+    </v-row>
+
+    <v-list-item>
       <v-list-item-content>
-        <v-list-item-title v-html="item.title"></v-list-item-title>
-        <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+        <div class="text-left mt-32">
+          <v-rating
+            v-model="item.rating"
+            color="yellow darken-3"
+            background-color="grey darken-1"
+            size="24"
+            readonly="true"
+            large
+          ></v-rating>
+          <v-list-item-title class="font-weight-bold" v-html="item.title"></v-list-item-title>
+          <v-list-item-subtitle v-html="item.reviewDate"></v-list-item-subtitle>
+        </div>
+
+        <v-list-item-content v-html="item.comment"></v-list-item-content>
       </v-list-item-content>
     </v-list-item>
 
@@ -30,9 +38,9 @@
 </template>
 
 
-  <script>
+<script>
   export default ({
-    props: ['name', 'department', 'occupation', 'joinedYear', 'phoneNumber'],
+    props: ['name', 'department', 'occupation', 'joinedYear'],
     data() {
       return {
         item: 
@@ -40,7 +48,8 @@
             avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
             rating: 4,
             title: 'スクラム導入中のチームが読むべき一冊',
-            subtitle: `<span class="text--primary">スクラムという仕事の進め方のフレームワークについて書かれた本。
+            reviewDate: '2023年2月10日にレビュー済み',
+            comment: `<span class="text--primary">スクラムという仕事の進め方のフレームワークについて書かれた本。
       特にゾンビ化（スクラムの体を成しているようだが、実際には伴っていない）に焦点を当てている点が興味深かった。
       スクラム導入中のチームが、自分たちのスクラムはうまく機能しているのかを見つめ直すのにオススメの1冊です。`,
           },
