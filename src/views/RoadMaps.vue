1178778911aaa96b5be6a3e0c1e9e3c1aac4618b
terashima.tatsuma <terashima.tatsuma.xe4@jp.nssol.nipponsteel.com>
2023-02-13 13:09:14 +0900

    ヘッダータブ追加

--- src/views/RoadMaps.vue
+++ src/views/RoadMaps.vue
@@ -44,13 +44,49 @@ const fetcher = (search) => {
     <Header></Header>
 
     <!-- 検索フォーム -->
-    <div>
+    <v-row no-gutters>
+      <v-col cols="1" v-show="searching">
+        <v-btn @click="cancelSearching" height="55">
+          <v-icon>mdi-arrow-left</v-icon>
+        </v-btn>
+      </v-col>
+      <v-col :cols="searching ? 10 : 11">
+        <v-form>
+          <v-text-field
+            prepend-inner-icon="mdi-magnify"
+            label="書籍検索"
+            placeholder="書籍名等を入力してください"
+            clear-icon="mdi-close-circle"
+            clearable
+          ></v-text-field>
+        </v-form>
+      </v-col>
+      <v-col cols="1">
+        <v-btn @click="Search" height="55">Search</v-btn>
+      </v-col>
+    </v-row>
+    <div v-if="!searching">
       <h2>New</h2>
       <BookSlideGroups :books="books"></BookSlideGroups>
       <br />
       <h2>おすすめ</h2>
       <BookSlideGroups :books="books"></BookSlideGroups>
       <v-divider class="mx-auto my-2"></v-divider>
+    </div>
+
+    <!-- 本一覧 -->
+    <div v-if="searching">
+      <h2>検索結果</h2>
+      <div v-for="book in books" :key="book">
+        <BookListItem :book="book" />
+      </div>
+      <!-- <div v-if="error">failed to load</div>
+        <div v-if="!data">loading...</div>
+        <div v-else>
+            <div v-for="book in data.items" :key="book">
+                <BookListItem :book=book />
+            </div>
+        </div> -->
     </div>
   </v-container>
 </template>
