197860280a2adbd6ff31b48ed5c85d231839597f
terashima.tatsuma <terashima.tatsuma.xe4@jp.nssol.nipponsteel.com>
2023-02-14 13:39:04 +0900

    ロードマップ一覧修正

--- src/components/RoadMapListItem.vue
+++ src/components/RoadMapListItem.vue
@@ -33,21 +33,36 @@ const closeRoadMapDialog = () => {
               <v-row>
                 <v-card-title class="text-h6">
                   {{ roadMap.title }}
-                  <!-- CI/CDマスターへの道 -->
                 </v-card-title>
               </v-row>
               <v-row class="mt-0">
                 <v-col cols="8" class="py-0">
                   <v-card-text class="py-0">
                     <v-list lines="one">
-                      <!-- <v-list-item title="貸出ステータス">
-                        <v-icon x-small :color="book.status ? 'green' : 'red' + ' darken-2'">
-                          mdi-moon-full
-                        </v-icon>
-                      </v-list-item>
-                      <v-list-item title="場所" :subtitle="book.location"></v-list-item> -->
+                      <v-list-item
+                        title="作成者"
+                        :subtitle="
+                          roadMap.user.occupation +
+                          ' ' +
+                          roadMap.user.joinedYear +
+                          '年入社'
+                        "
+                      ></v-list-item>
                     </v-list>
                   </v-card-text>
+                </v-col>
+                <v-col cols="4">
+                  <v-rating
+                    v-model="roadMap.avarageRating"
+                    color="yellow darken-3"
+                    background-color="grey darken-1"
+                    size="24"
+                    readonly="true"
+                    large
+                  >
+                  </v-rating>
+                  <br />
+                  {{ roadMap.review_num }}件のレビュー
                 </v-col>
                 <v-col cols="4" align-self="center"> </v-col>
               </v-row>
