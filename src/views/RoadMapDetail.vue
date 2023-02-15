b4d181cf076c0621a0c09f6f3170c6800713bdd4
terashima.tatsuma <terashima.tatsuma.xe4@jp.nssol.nipponsteel.com>
2023-02-15 16:53:17 +0900

    ロードマップ詳細画面修正

--- src/views/RoadMapDetail.vue
+++ src/views/RoadMapDetail.vue
@@ -70,37 +70,24 @@ const openBookDetailPage = (bookId) => {
         </v-row>
         <v-divider></v-divider>
 
-        <v-timeline side="end" density="comfortable" class="mb-3">
-          <v-timeline-item
-            :dot-color="book.status ? 'teal-lighten-3' : 'blue-grey-lighten-4'"
-            :size="book.status ? 'default' : 'small'"
-            min-width="90%"
-            v-for="book in books"
-            :key="book"
-          >
-            <BookListItem
-              :book="book"
-              :width="1000"
-              :elevation="0"
-              @click="openBookDetailPage(book.id)"
-            ></BookListItem>
-            <!-- <v-col cols="5">
-              <v-img class="ml-auto my-auto" max-height="150" max-width="150"
-                :src="book.volumeInfo.imageLinks.smallThumbnail">
-              </v-img>
-            </v-col>
-            <v-col cols="7">
-              <div>
-                <strong>{{ book.volumeInfo.title }}</strong>
-                <div class="text-caption">
-                  {{ book.status ? "貸出可能" : "貸出不可" }}
-                  <v-icon x-small :color="book.status ? 'green' : 'red' + ' darken-2'">
-                    mdi-moon-full </v-icon><br />
-                </div>
-              </div>
-            </v-col> -->
-          </v-timeline-item>
-        </v-timeline>
+        <v-col
+          ><v-timeline density="comfortable" truncate-line="both">
+            <v-timeline-item
+              :dot-color="
+                book.status ? 'teal-lighten-3' : 'blue-grey-lighten-4'
+              "
+              :size="book.status ? 'default' : 'small'"
+              v-for="book in books"
+              :key="book"
+            >
+              <BookListItem
+                :book="book"
+                :width="1000"
+                :elevation="0"
+                @click="openBookDetailPage(book.id)"
+              ></BookListItem>
+            </v-timeline-item> </v-timeline
+        ></v-col>
 
         <v-divider></v-divider>
 
