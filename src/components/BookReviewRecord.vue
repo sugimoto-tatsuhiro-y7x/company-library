ab1767904b93ceb032ec4278481b08f0c3f2da5c
terashima.tatsuma <terashima.tatsuma.xe4@jp.nssol.nipponsteel.com>
2023-02-16 10:58:53 +0900

    不要なコメント削除

--- src/components/BookReviewRecord.vue
+++ src/components/BookReviewRecord.vue
@@ -9,9 +9,6 @@ defineProps({
   reviewBooks: Object,
 });
 
-// const goodFlg = ref(false);
-// const goodNum = ref(0);
-
 const clickGood = (item) => {
   if (item.goodFlg) {
     item.goodFlg = false;
@@ -37,52 +34,6 @@ const clickGood = (item) => {
       :key="n"
     >
       <BookReviewRecordItem :review="item"></BookReviewRecordItem>
-      <!-- <v-divider v-if="n != 0" class="my-1"></v-divider>
-
-      <v-card mx-auto>
-        <v-row>
-          <v-col cols="1">
-            <v-list-item-avatar>
-              <UserAvatar :userId="n + 1"></UserAvatar>
-            </v-list-item-avatar>
-          </v-col>
-          <v-col cols="10">
-            <div>{{ item.occupation }}</div>
-            <div>{{ item.joinedYear }}入社</div>
-          </v-col>
-          <v-col cols="1">
-            <v-btn
-              size="x-small"
-              elevation="0"
-              :color="red"
-              @click="clickGood(item)"
-              ><v-icon :color="item.goodFlg ? '#E53935' : '#ECEFF1'">
-                {{ item.goodFlg ? "mdi-heart" : "mdi-heart-outline" }}</v-icon
-              >
-              <div style="font-size: 50%">{{ item.goodNum }}</div></v-btn
-            >
-          </v-col>
-        </v-row>
-        <div class="text-left mt-32">
-          <v-rating
-            v-model="item.rating"
-            color="yellow darken-3"
-            background-color="grey darken-1"
-            size="24"
-            readonly="true"
-            large
-          ></v-rating>
-        </div>
-        <v-list-item-title
-          class="font-weight-bold"
-          v-html="item.reviewTitle"
-        ></v-list-item-title>
-        <v-list-item-subtitle v-html="item.reviewDate"></v-list-item-subtitle>
-        <v-list-item-title
-          class="wrap-text text--darken-1"
-          v-html="item.comment"
-        ></v-list-item-title>
-      </v-card> -->
     </v-list-item>
   </v-list>
   <v-card v-else style="margin: 10 20px 0 0; width: 400px">
